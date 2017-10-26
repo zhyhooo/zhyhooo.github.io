8224168e4bd729738a0387d8164ffb181697756a
zhyhooo <zhyh8341@gmail.com>
2017-10-26 15:33:20 +0800

    add new page in 3js

---
+++ 3js/files.js
@@ -0,0 +1,32 @@
+var files = {
+	"shrubs": [
+		"example",
+		"example",
+		"example",
+		"example",
+		"example"
+	],
+	"trees": [
+		"Ash",
+		"Birch",
+		"Butternut",
+		"Elm",
+		"Hickory",
+		"Locust",
+		"Maple",
+		"Oak"
+	],
+	"vines": [
+		"example",
+		"example"
+	],
+	"succulent plants": [
+		"example",
+		"example"
+	],
+	"herbaceous plants": [
+		"example",
+		"example",
+		"example"
+	]
+};
